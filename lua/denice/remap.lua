6781dbeecbadd701982dca079c875155273c08df
Dennis Koskei <89400168+DennisKoskei@users.noreply.github.com>
2024-12-09 23:39:39 +0300

    Updated the keymaps correcly and Add new Plugins

--- lua/denice/remap.lua
+++ lua/denice/remap.lua
@@ -1,22 +1,46 @@
 -- [[ Basic Keymaps ]]
-vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>") --> MyNote:(come to this command and set it to desired state)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
--- vim.keymap.set("n", "q", "<Esc>")
--- vim.keymap.set("n", "jj", "<Esc>")
-vim.keymap.set("n", "<C-qq", function()
-	print("Take some more juice pleaseeeeeeeeee !!!!1")
-end) --> Add my own text keymap
---> Add a keymap which will save the document whenever you hit  Ctrl + Enter`.
---vim.keymap.set("i", "<C-e", "<Esc>diwa")
---
--- some random test that I am typing and then something -s
+
+-- [[ TRY TO REMAP THESE KEYS ]]
+-- 1. Add kaymap to save Add a keymap which will save the document whenever you hit  Ctrl + Enter`.
+-- 2. Add keymap to exit out of Telescope
+-- 3. Integrate same button to Toggle entry into Neo-tree and out of it.
+-- vim.keymap.set("n", "<leader>qq", "<Esc><Esc>")
+
+vim.keymap.set("n", "<leader>n", function()
+	vim.cmd("Neotree")
+end, { desc = "Open Neotree Navigation panel" })
+
+vim.keymap.set("n", "<leader>l", function()
+	vim.cmd("Lazy")
+end, { desc = "Open Lazy" })
+
+vim.keymap.set("n", "<leader>m", function()
+	vim.cmd("Mason")
+end, { desc = "Open Mason" })
+
+-- <Esc>
+vim.keymap.set("n", "<leader>e", "<Esc><Esc>", { desc = "Exit Telescope" })
+
+vim.keymap.set("n", "<leader>n", "<cmd>nohlsearch<CR>") --> MyNote:(come to this command and set it to desired state)
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>') -- TIP: Disable arrow keys in normal mode
+
+-- Duhh.. !!!
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" }) -- Keybinds to make split navigation easier.
+
+-- Keybinds to make split navigation easier.
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+vim.keymap.set("n", "<leader><leader>", function()
+	vim.cmd("so")
+end, { desc = "Source file " })
+
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")
--- lua/denice/set.lua
+++ lua/denice/set.lua
@@ -3,26 +3,40 @@ vim.g.maplocalleader = " "
 
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = "a"
+vim.opt.scrolloff = 15
+vim.opt.colorcolumn = "80"
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.backup = false
+vim.opt.swapfile = false
+-- vim.opt.undodir = os.getenv("HOME") .. "/nvim/undodir"
+-- vim.opt.undofile = true
+
 vim.opt.showmode = false
 vim.schedule(function()
-	vim.opt.clipboard = "unnamedplus"
+	vim.opt.clipboard = "unnamedplus" -- This is used to set a global clipboard (instead of having a buffer).
 end)
 vim.opt.breakindent = true
-vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.listchars = { tab = "»» ", trail = "·", nbsp = "␣" }
 vim.opt.inccommand = "split"
 vim.opt.cursorline = true
-vim.opt.scrolloff = 15
--- vim. -- Show the 79 limit char for writing
+
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 50
