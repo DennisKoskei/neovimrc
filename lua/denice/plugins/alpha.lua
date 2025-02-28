bd97b3f1db8e08e1e9f8ca2f52ca80569d4ddfd4
Dennis Koskei <89400168+DennisKoskei@users.noreply.github.com>
2025-02-28 16:28:17 +0300

    Mod alpha.lua
    Make alpha.lua header to have color

--- lua/denice/plugins/alpha.lua
+++ lua/denice/plugins/alpha.lua
@@ -27,19 +27,40 @@ return {
 			"            ──────────────────────────────────────────────────────            ",
 		}
 
+		dashboard.section.header.opts.hl = "AlphaHeader"
+		dashboard.section.header.opts.position = "center"
+		dashboard.section.footer.opts.position = "center"
+
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("SPC pf", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC n", "  > Toggle file explorer", "<cmd>Neotree<CR>"),
-			dashboard.button("SPC pr", "󰱼  > Find Recent Files", "<cmd>Telescope oldfiles<CR>"),
-			dashboard.button("SPC pg", "  > Find Text", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("SPC pf", " " .. " Find file ", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("e", " " .. " New file ", "<cmd>ene<CR>"),
+			dashboard.button("SPC n", " " .. " Toggle file explorer ", "<cmd>Neotree<CR>"),
+			dashboard.button("SPC pr", " " .. " Recent files ", "<cmd>Telescope oldfiles<CR>"),
+			dashboard.button("SPC pg", " " .. " Find text ", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC wr", "󰁯 " .. " Restore Session ", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", " " .. " Quit ", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
 		alpha.setup(dashboard.opts)
+
+		vim.api.nvim_create_autocmd("User", {
+			once = true,
+			pattern = "LazyVimStarted",
+			callback = function()
+				local stats = require("lazy").stats()
+				local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+				dashboard.section.footer.val = "⚡ Neovim loaded "
+					.. stats.loaded
+					.. "/"
+					.. stats.count
+					.. " plugins in "
+					.. ms
+					.. "ms"
+				pcall(vim.cmd.AlphaRedraw)
+			end,
+		})
 
 		-- Disable folding on alpha buffer
 		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
