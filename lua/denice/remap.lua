70d8086fa8e9171c13312473a1c31f5f39a6cb90
Dennis Koskei <89400168+DennisKoskei@users.noreply.github.com>
2026-03-28 23:18:03 +0300

    remap-mod
    1. Remove Open Hexplore remap
    2. Fixed Yank document and restore cursor to original position
    3. Deleted repetitive (scroll half page down) remap
    4. Add description to nohlsearch

--- lua/denice/remap.lua
+++ lua/denice/remap.lua
@@ -1,30 +1,26 @@
 local keymap = vim.keymap
 
--- keymap.set("n", "<leader>pp", vim.cmd.Ex, { desc = "Show Hexplore" })
 keymap.set("i", "jj", "<Esc>", { desc = "Switch to Normal mode" })
+keymap.set("i", "JJ", "<Esc>", { desc = "Switch to Normal mode" })
 
-keymap.set("n", "<C-d", "<C-d>zz", { desc = "Scroll document 1/2 page down, keeping cursor at  the center" })
-keymap.set("n", "<C-u", "<C-u>zz", { desc = "Scroll document 1/2 page up, keeping cursor at  the center" })
+keymap.set("n", "<C-d", "<C-d>zz", { desc = "Scroll half page down and center cursor" })
+keymap.set("n", "<C-u", "<C-u>zz", { desc = "Scroll half up down and center cursor" })
 
 keymap.set("n", "x", '"_x', { desc = "Doesn't store cut character" })
-
-keymap.set("n", "n", "nzzzv", { desc = "Scroll half page down and center cursor" })
-keymap.set("n", "N", "Nzzzv", { desc = "Scroll half up down and center cursor" })
 
 keymap.set("v", "J", ":m '<+1<CR>gv=gv", { desc = "Move highlighed sentence down" })
 keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move highlighed sentence up" })
 
-keymap.set("n", "yd", "gg0yGgg", { desc = "Yank Entire [d]ocument" }) -- INFO: Search for a way to keep the cursor at same position
--- keymap.set("n", "yD", "ggVG<CR>", { desc = "Highlight Entire [d]ocument" }) -- INFO: Make this work
-vim.keymap.set("n", "<leader>v", "ggVG", { desc = "Select entire document" })
+keymap.set("n", "yd", "mygg0yG`y", { desc = "Yank Entire [d]ocument and return cursor" })
+keymap.set("n", "<leader>v", "ggVG", { desc = "Select entire document" })
 
-keymap.set("n", "\\", "<cmd>Neotree reveal<CR>", { desc = "Open [N]eotree Navigation panel" })
+keymap.set("n", "\\", "<cmd>Neotree reveal<CR>", { desc = "Open Neotree Navigation panel" })
 keymap.set("n", "<leader>e", "<cmd>Neotree close<CR>", { desc = "Close Neotre[e] Navigation panel" })
 
 keymap.set("n", "<leader>la", "<cmd>Lazy<CR>", { desc = "Open Lazy" })
 keymap.set("n", "<leader>ma", "<cmd>Mason<CR>", { desc = "Open Mason" })
 
-keymap.set("n", "<C-n>", "<cmd>nohlsearch<CR>")
+keymap.set("n", "<C-n>", "<cmd>nohlsearch<CR>", { desc = "Clear search Highlights" })
 
 vim.diagnostic.config({
 	update_in_insert = false,
@@ -61,4 +57,4 @@ keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window"
 -- keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
-keymap.set("n", "<leader><leader>", ":source<CR>", { desc = "Source file " })
+keymap.set("n", "<leader>o", ":source<CR>", { desc = "S[o]urce file" })
